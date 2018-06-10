299b3c1ec947cf316abaff9ad8f0531863cd20f7
Sam Watts <samuel.watts@gmail.com>
2018-06-10 12:04:51 +0100

    fetch and show balance in detail view

---
+++ PRDemoApp/Screens/Card Detail/CardDetailViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  CardDetailViewModel.swift
+//  PRDemoApp
+//
+//  Created by Sam Watts on 10/06/2018.
+//  Copyright © 2018 Sam Watts. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+final class CardDetailViewModel {
+
+    let disposeBag = DisposeBag()
+
+    let cardDetails = PublishSubject<CardDetail>()
+    private let card: Card
+
+    let dataProvider: DataProvider = Current.dataProvider
+
+    init(card: Card) {
+        self.card = card
+    }
+
+    func fetchCardDetails() {
+
+        dataProvider
+            .fetchResponse(fromURL: URL.cardDetail(forCard: card))
+            .convert(to: CardDetail.self)
+            .observeOn(MainScheduler.instance)
+            .bind(to: cardDetails)
+            .disposed(by: disposeBag)
+    }
+}
