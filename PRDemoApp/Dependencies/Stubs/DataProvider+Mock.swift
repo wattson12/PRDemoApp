66bb8b72dec1613625dac8814dc46f569bf73032
Sam Watts <samuel.watts@gmail.com>
2018-06-10 12:18:23 +0100

    Added some tests

--- PRDemoApp/Dependencies/Stubs/DataProvider+Mock.swift
+++ PRDemoApp/Dependencies/Stubs/DataProvider+Mock.swift
@@ -9,17 +9,40 @@
 import Foundation
 import RxSwift
 
-struct StubProvider<T: Encodable>: DataProvider {
-
-    let value: T
+struct StubProvider: DataProvider {
 
     func fetchResponse(fromURL url: URL) -> Observable<Data> {
+        if url == URL.cardList {
+            return fetchCardList()
+        } else if url == URL.cardDetail(forCard: Card(identifier: 1, name: "")) {
+            return fetchCardDetails()
+        } else {
+            fatalError()
+        }
+    }
+
+    private func returnContent<T: Codable>(_ value: T) -> Observable<Data> {
         do {
             let data = try JSONEncoder().encode(value)
             return .just(data)
         } catch {
             return .error(error)
         }
+    }
+
+    private func fetchCardList() -> Observable<Data> {
+        let values: [Card] = [
+            Card(identifier: 1, name: "first card"),
+            Card(identifier: 2, name: "another card"),
+            Card(identifier: 3, name: "last card")
+        ]
+
+        return returnContent(values)
+    }
+
+    private func fetchCardDetails() -> Observable<Data> {
+        let cardDetails = CardDetail(identifier: 1, name: "detail name", balance: "$500")
+        return returnContent(cardDetails)
     }
 }
 
@@ -32,11 +55,6 @@ struct StubDataProvider: DataProvider {
     }
 }
 
-var mockDataProvider: StubProvider<[Card]> {
-    let values: [Card] = [
-        Card(identifier: 1, name: "first card"),
-        Card(identifier: 2, name: "another card"),
-        Card(identifier: 3, name: "last card")
-    ]
-    return StubProvider(value: values)
+var mockDataProvider: StubProvider {
+    return StubProvider()
 }
