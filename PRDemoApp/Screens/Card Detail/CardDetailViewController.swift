688312d88cd5a3f4a78847c83bc72b44a3dc1af5
Sam Watts <samuel.watts@gmail.com>
2018-06-10 12:04:51 +0100

    fetch and show balance in detail view

--- PRDemoApp/Screens/Card Detail/CardDetailViewController.swift
+++ PRDemoApp/Screens/Card Detail/CardDetailViewController.swift
@@ -10,9 +10,54 @@ import UIKit
 
 class CardDetailViewController: BaseViewController {
 
+    let balanceLabel: UILabel = {
+        let balanceLabel = UILabel()
+        balanceLabel.translatesAutoresizingMaskIntoConstraints = false
+        balanceLabel.textAlignment = .center
+        return balanceLabel
+    }()
+
+    let viewModel: CardDetailViewModel
+
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+
+        view.addSubview(balanceLabel)
+
+        NSLayoutConstraint.activate([
+            balanceLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            balanceLabel.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor)
+        ])
+    }
+
     init(card: Card) {
+        viewModel = CardDetailViewModel(card: card)
+
         super.init()
 
         title = card.name
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupBindings()
+    }
+
+    private func setupBindings() {
+
+        viewModel
+            .cardDetails
+            .subscribe(onNext: { [weak self] cardDetails in
+                self?.balanceLabel.text = cardDetails.balance
+            })
+            .disposed(by: disposeBag)
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        viewModel.fetchCardDetails()
     }
 }
