4afee88de6fc36ab13d112a19acf9ffdc877c3ca
Sam Watts <samuel.watts@gmail.com>
2018-06-10 11:00:23 +0100

    Added a data provider

---
+++ PRDemoApp/Model/Card.swift
@@ -0,0 +1,14 @@
+//
+//  Card.swift
+//  PRDemoApp
+//
+//  Created by Sam Watts on 10/06/2018.
+//  Copyright © 2018 Sam Watts. All rights reserved.
+//
+
+import Foundation
+
+struct Card: Codable {
+    let identifier: Int
+    let name: String
+}
